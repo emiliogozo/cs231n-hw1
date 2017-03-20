6830b4105a0dfaf0a27598608348fe82be30a509
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-03-20 12:33:12 +0800

    feat: Implemented LinearClassifier.predict

--- cs231n/classifiers/linear_classifier.py
+++ cs231n/classifiers/linear_classifier.py
@@ -71,15 +71,7 @@ class LinearClassifier(object):
           array of length N, and each element is an integer giving the predicted
           class.
         """
-        y_pred = np.zeros(X.shape[1])
-        #######################################################################
-        # TODO:                                                                   #
-        # Implement this method. Store the predicted labels in y_pred.            #
-        #######################################################################
-        pass
-        #######################################################################
-        #                           END OF YOUR CODE                              #
-        #######################################################################
+        y_pred = np.argmax(X.dot(self.W), axis=1)
         return y_pred
 
     def loss(self, X_batch, y_batch, reg):
